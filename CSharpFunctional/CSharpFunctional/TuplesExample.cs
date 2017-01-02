1d547ed5eb54b0fccf5f9908f94e1db798647cd2
Ajit Chahal <Ajit Chahal>
2017-01-02 03:41:34 +0100

    added tuple example

---
+++ CSharpFunctional/CSharpFunctional/TuplesExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFunctional
+{
+    public class TuplesExample
+    {
+        public void createATuple() {
+            var tuple = Tuple.Create(1,2,3,4,5);
+        }
+    }
+}
